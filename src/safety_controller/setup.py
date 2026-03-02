386fd20ca4a8f4c1a30e0e03ed8f771368d37623
Ansis <ansisg@mit.edu>
2026-03-02 00:15:52 -0500

    Initial safety controller

---
+++ src/safety_controller/setup.py
@@ -0,0 +1,36 @@
+import glob
+import os
+
+from setuptools import find_packages, setup
+
+package_name = "safety_controller"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml", "safety_controller/params.yaml"]),
+        (
+            "share/safety_controller/launch",
+            glob.glob(os.path.join("launch", "*launch.xml")),
+        ),
+        (
+            "share/safety_controller/launch",
+            glob.glob(os.path.join("launch", "*launch.py")),
+        ),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="racecar",
+    maintainer_email="ansisg@mit.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "safety_controller = safety_controller.safety_controller:main"
+        ],
+    },
+)
