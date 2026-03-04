cc354822d26e38d3e73033234fd219164a989367
Ansis <ansisg@mit.edu>
2026-03-04 16:11:50 -0500

    bugfix

--- src/wall_follower_sim/wall_follower/wall_follower.py
+++ src/wall_follower_sim/wall_follower/wall_follower.py
@@ -128,7 +128,7 @@ class WallFollower(Node):
     def scan_callback(self, laser_scan):
         walls = detect_walls(laser_scan, min_points=3, D_t=0.5)
         # self.visualization_tools.plot_walls(walls, laser_scan.header.stamp)
-        self.drive_controller.update(walls, laser_scan)
+        self.drive_controller.update(walls)
 
 
 def main():
