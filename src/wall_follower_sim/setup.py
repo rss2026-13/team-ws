18747f3a2eaad3121d907394f3d501c0ffc9f32a
Muktha Ramesh <muktharamesh20@gmail.com>
2026-03-06 16:40:15 -0500

    updated setup.py again

--- src/wall_follower_sim/setup.py
+++ src/wall_follower_sim/setup.py
@@ -1,36 +1,32 @@
 import glob
 import os
+from setuptools import find_packages
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-package_name = "wall_follower"
+package_name = 'wall_follower'
 
 setup(
     name=package_name,
-    version="0.0.0",
-    packages=find_packages(exclude=["test"]),
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml", "safety_controller/params.yaml"]),
-        (
-            "share/safety_controller/launch",
-            glob.glob(os.path.join("launch", "*launch.xml")),
-        ),
-        (
-            "share/safety_controller/launch",
-            glob.glob(os.path.join("launch", "*launch.py")),
-        ),
-    ],
-    install_requires=["setuptools"],
+        ('share/'+package_name, ['package.xml', "wall_follower/params.yaml"]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/wall_follower/launch', glob.glob(os.path.join('launch', '*launch.xml'))),
+        ('share/wall_follower/launch', glob.glob(os.path.join('launch', '*launch.py')))],
+
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="racecar",
-    maintainer_email="ansisg@mit.edu",
-    description="TODO: Package description",
-    license="TODO: License declaration",
-    tests_require=["pytest"],
+    maintainer='Sebastian',
+    maintainer_email='sebastianag2002@gmail.com',
+    description='Wall Follower ROS2 Package',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
     entry_points={
-        "console_scripts": [
-            'wall_follower = wall_follower.wall_follower:main'
+        'console_scripts': [
+            'wall_follower = wall_follower.wall_follower:main',
+        	'test_wall_follower = wall_follower.test_wall_follower:main',
         ],
     },
 )
