fcfda510fbf4f07bd308beb179552ea31ed0ffaa
Ylewtro <jeryllewiscs@gmail.com>
2026-03-09 06:54:39 +0000

    safety updates

--- src/safety_controller/safety_controller/safety_controller.py
+++ src/safety_controller/safety_controller/safety_controller.py
@@ -19,9 +19,8 @@ class SafetyController(Node):
         self.declare_parameter("margin", 0.2)
         self.declare_parameter("max_deceleration", 2.0)
         self.declare_parameter("car_width", 0.25)
-        self.declare_parameter("car_length", 0.325)
+        self.declare_parameter("wheelbase", 0.325)
         self.declare_parameter("lidar_offset", 0.12)  # Distance from lidar to front bumper
-        self.declare_parameter("base_to_lidar", 0.04)
         self.declare_parameter("visualize", True)
 
         self.DRIVE_TOPIC = self.get_parameter("drive_topic").get_parameter_value().string_value
@@ -30,9 +29,8 @@ class SafetyController(Node):
         self.MARGIN = self.get_parameter("margin").get_parameter_value().double_value
         self.MAX_DECELERATION = self.get_parameter("max_deceleration").get_parameter_value().double_value
         self.CAR_WIDTH = self.get_parameter("car_width").get_parameter_value().double_value
-        self.CAR_LENGTH = self.get_parameter("car_length").get_parameter_value().double_value
+        self.WHEELBASE = self.get_parameter("wheelbase").get_parameter_value().double_value
         self.LIDAR_OFFSET = self.get_parameter("lidar_offset").get_parameter_value().double_value
-        self.BASE_TO_LIDAR = self.get_parameter("base_to_lidar").get_parameter_value().double_value
         self.VISUALIZE = self.get_parameter("visualize").get_parameter_value().bool_value
 
         self.drive_subscription = self.create_subscription(
@@ -52,8 +50,8 @@ class SafetyController(Node):
         self.drive_command = None
         self.scan_cos_angles = None
         self.scan_sin_angles = None
-        self.collision_counter = 0
-        self.DETECTION_THRESHOLD = 3
+        self.collision_counter = 0 # Needed to account for salt and pepper LiDAR noise
+        self.DETECTION_THRESHOLD = 3 # Number of frames scan must remain in collision zone for stop to occur
 
     def drive_callback(self, msg):
         self.drive_command = msg
@@ -61,7 +59,7 @@ class SafetyController(Node):
         self.evaluate_safety()
 
     def scan_callback(self, msg):
-        if (self.scan_cos_angles is None) or (self.scan_sin_angles is None):
+        if (self.scan_cos_angles is None) or (self.scan_sin_angles is None): # Lazy initialization
             angles = np.linspace(msg.angle_min, msg.angle_max, num=np.array(msg.ranges).shape[0])
             self.scan_cos_angles = np.cos(angles)
             self.scan_sin_angles = np.sin(angles)
@@ -78,25 +76,26 @@ class SafetyController(Node):
         if velocity < 0.001:
             return
 
-        # 1. Kinematic Threshold
+        # Kinematic Threshold
         front_threshold = self.MARGIN + (velocity ** 2) / (2 * self.MAX_DECELERATION)
         delta = self.drive_command.drive.steering_angle
         if self.VISUALIZE:
             self.publish_safety_marker(delta, front_threshold)
 
-        # 2. Get Cartesian points
+        # Get Cartesian points
         ranges = np.array(self.scan_data.ranges)
         px = ranges * self.scan_cos_angles
         py = ranges * self.scan_sin_angles
 
+        # Publish distance from front bumper
         dist_x = px - self.LIDAR_OFFSET
         mask = (np.abs(py) < self.CAR_WIDTH/2) & (dist_x > 0)
         if np.any(mask):
             distance = Float32()
             distance.data = float(np.median(dist_x[mask])) 
             self.distance_pub.publish(distance)
-        # 3. Adjust for LIDAR offset (Move points to car's front bumper frame)
-        # If px=0 is the LIDAR, then the bumper is at self.LIDAR_OFFSET
+
+        # Adjust for LIDAR offset (Move points to car's front bumper frame)
         # We check if points are within 'front_threshold' OF THE BUMPER
         collision_zone_start = self.LIDAR_OFFSET
         collision_zone_end = self.LIDAR_OFFSET + front_threshold
@@ -110,29 +109,28 @@ class SafetyController(Node):
                     (np.abs(py) < self.CAR_WIDTH / 2)
             self.is_collision = np.any(in_path)
         else: # Curved Path (Bicycle Model)
-            # Note: This assumes LIDAR is at (L - offset, 0) relative to rear axle
-            # and CAR_LENGTH is the wheelbase.
-            R = self.CAR_LENGTH / np.tan(delta)
-            R_max = np.sqrt(self.CAR_LENGTH**2 + (abs(R) + self.CAR_WIDTH/2)**2)
+            R = self.WHEELBASE / np.tan(delta) # Radius of car rotation, max and min account for front and back corner
+            R_max = np.sqrt((self.WHEELBASE + self.LIDAR_OFFSET)**2 + (abs(R) + self.CAR_WIDTH/2)**2)
             R_min = abs(R) - self.CAR_WIDTH/2
 
-            px_cor = px + self.CAR_LENGTH - self.LIDAR_OFFSET
+            # Points in car center of rotation (cor) frame
+            px_cor = px + self.WHEELBASE
             py_cor = py - R
             pr = np.sqrt((px_cor)**2 + (py_cor)**2)
-            
-            # Arc length check
             pangle = np.mod(np.arctan2(px_cor, py_cor * -np.sign(R)), 2 * np.pi)
             
-            bumper_angle = np.arcsin(np.clip(self.CAR_LENGTH / pr, -1.0, 1.0))
+            # Angle that bumper is at in point's cor frame
+            bumper_angle = np.arcsin(np.clip((self.WHEELBASE + self.LIDAR_OFFSET) / pr, -1.0, 1.0))
             p_bumper_ahead_dist = (pangle - bumper_angle) * pr 
             
-            # Adjusting arc check: Obstacle must be past the bumper 
-            # but within stopping distance
+            # Obstacle must be within the car's curved path
+            # Obstacle must be past the bumper but within stopping distance
             in_path = (pr > R_min) & (pr < R_max) & \
                       (p_bumper_ahead_dist > 0) & (p_bumper_ahead_dist < front_threshold)
             
             self.is_collision = np.any(in_path)
         
+        # Ignore salt and pepper LiDAR noise
         if self.is_collision:
             self.collision_counter += 1
         else:
@@ -175,7 +173,7 @@ class SafetyController(Node):
             # --- Straight Case: Two Parallel Lines ---
             y_inner = -self.CAR_WIDTH / 2
             y_outer = self.CAR_WIDTH / 2
-            x_start = 0.0 
+            x_start = self.LIDAR_OFFSET 
             x_end = x_start + stop_dist
             
             # Left Boundary
@@ -190,9 +188,9 @@ class SafetyController(Node):
             
         else:
             # --- Curved Case: Swept Path (Bicycle Model) ---
-            R = self.CAR_LENGTH / np.tan(delta)
+            R = self.WHEELBASE / np.tan(delta)
             R_min = abs(R) - self.CAR_WIDTH / 2
-            R_max = np.sqrt(self.CAR_LENGTH**2 + (abs(R) + self.CAR_WIDTH / 2)**2)
+            R_max = np.sqrt((self.WHEELBASE + self.LIDAR_OFFSET)**2 + (abs(R) + self.CAR_WIDTH / 2)**2)
             
             # Helper to calculate points along an arc for a specific radius
             def get_arc_point(radius, current_arc_dist, theta_start):
@@ -208,8 +206,8 @@ class SafetyController(Node):
                 return self.create_point(x, y)
 
 
-            theta_start_min = np.arcsin(np.clip(self.CAR_LENGTH / R_min, -1.0, 1.0))
-            theta_start_max = np.arcsin(np.clip(self.CAR_LENGTH / R_max, -1.0, 1.0))
+            theta_start_min = np.arcsin(np.clip((self.WHEELBASE + self.LIDAR_OFFSET) / R_min, -1.0, 1.0))
+            theta_start_max = np.arcsin(np.clip((self.WHEELBASE + self.LIDAR_OFFSET) / R_max, -1.0, 1.0))
             # Generate segments for both inner and outer boundaries
             for i in range(num_steps):
                 d1 = (i / num_steps) * stop_dist
