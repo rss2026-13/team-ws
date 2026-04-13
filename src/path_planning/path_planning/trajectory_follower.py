b1898265aa709bbdf45fc496ee47e33dc7f28faa
tissanyc <tissanyc999@gmail.com>
2026-04-13 18:03:35 -0400

    Update trajectory_follower.py to have eval topics

--- src/path_planning/path_planning/trajectory_follower.py
+++ src/path_planning/path_planning/trajectory_follower.py
@@ -2,6 +2,8 @@ import rclpy
 
 from ackermann_msgs.msg import AckermannDriveStamped
 from geometry_msgs.msg import PoseArray
+from visualization_msgs.msg import Marker
+from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
 from rclpy.node import Node
 from .utils import LineTrajectory
@@ -25,6 +27,7 @@ class PurePursuit(Node):
         self.k_curv = 1.0
         self.speed = 4.0  # FILL IN #
         self.wheelbase_length = 0.325  # FILL IN #
+        self.goal_threshold = 0.5
 
         self.starting_points = 0
         self.ending_points  = 0 
@@ -47,16 +50,30 @@ class PurePursuit(Node):
         self.drive_pub = self.create_publisher(AckermannDriveStamped,
                                                self.drive_topic,
                                                1)
+        self.cte_pub = self.create_publisher(
+            Float32,
+            "/trajectory/cte",
+            10 
+        )
+        self.heading_error_pub = self.create_publisher(
+            Float32,
+            "/trajectory/heading_error",
+            10 
+        )
+        self.lookahead_pub = self.create_publisher(
+            Float32,
+            "/trajectory/lookahead",
+            10 
+        )
+        self.steering_angle_pub = self.create_publisher(
+            Float32,
+            "/trajectory/steering_angle",
+            10 
+        )
  
 
     def pose_callback(self, odometry_msg):
         """
-        Need to add evaluation for:
-        1. Cross track Error (distance_to_segments[min_dist_idx])
-        2. Heading Error (car_theta - closest segment angle)
-        3. Lookahead distance over time / Curvature over time
-        4. Steering angle over time
-
         Need to add visualization for:
         1. lookahead point
         2. steering angle
@@ -73,20 +90,43 @@ class PurePursuit(Node):
         t = -np.einsum("ij,ij,i->i", self.segments, car_to_starting, self.inv_segments_mag_sq)
         t = np.clip(t, 0.0, 1.0)
         projections = self.starting_points + t[:, np.newaxis] * self.segments
-        distance_to_segments = np.linalg.norm(projections - car_position, axis=1)
+        car_to_segment = projections - car_position
+        distance_to_segments = np.linalg.norm(car_to_segment, axis=1)
         min_dist_idx = np.argmin(distance_to_segments)
 
+        # Publish cross track error
+        cte_float = Float32()
+        cte_float.data = distance_to_segments[min_dist_idx]
+        self.cte_pub.publish(cte_float)
+
+        # Publish heading error
+        segment_angle = np.arctan2(self.segments[min_dist_idx][1], self.segments[min_dist_idx][0])
+        heading_error = car_theta - segment_angle
+        # Wrap to [-pi, pi]
+        heading_error = (heading_error + np.pi) % (2 * np.pi) - np.pi
+        heading_error_float = Float32()
+        heading_error_float.data = heading_error
+        self.heading_error_pub.publish(heading_error_float)
+
         # Compute the lookahead distance
-        self.calculate_lookahead_distance(min_dist_idx)
+        self.compute_lookahead_distance(min_dist_idx)
+
+        # Publish lookahead distance
+        lookahead_float = Float32()
+        lookahead_float.data = self.lookahead
+        self.lookahead_pub.publish(lookahead_float)
 
         # Compute the lookahead point
         lookahead_point = self.find_lookahead_point(car_to_starting, min_dist_idx, t[min_dist_idx])
 
         # Give pure pursuit drive command
-        self.pub_pure_pursuit_drive_msg(lookahead_point, car_position, car_theta)
+        if (np.allclose(lookahead_point, self.ending_points[-1])) and (np.linalg.norm(self.ending_points[-1] - car_position) < self.goal_threshold):
+            self.pub_pure_pursuit_drive_msg(lookahead_point, 0.0, car_position, car_theta)
+        else:
+            self.pub_pure_pursuit_drive_msg(lookahead_point, self.speed, car_position, car_theta)
 
 
-    def calculate_lookahead_distance(self, start_idx, n_segments=3, decay = 0.5):
+    def compute_lookahead_distance(self, start_idx, n_segments=3, decay = 0.5):
         end_idx = min(start_idx + n_segments, len(self.segments) - 1)
 
         cos_angles = np.sum(self.normalized_segments[start_idx:end_idx] * self.normalized_segments[start_idx + 1 : end_idx + 1], axis=1)
@@ -126,7 +166,7 @@ class PurePursuit(Node):
         return self.starting_points[min_dist_idx] + (t_on_closest * self.segments[min_dist_idx])
     
 
-    def pub_pure_pursuit_drive_msg(self, lookahead_point, car_position, car_theta):
+    def pub_pure_pursuit_drive_msg(self, lookahead_point, speed, car_position, car_theta):
         dx = lookahead_point[0] - car_position[0]
         dy = lookahead_point[1] - car_position[1]
 
@@ -136,10 +176,15 @@ class PurePursuit(Node):
         ref_angle = np.arctan2(car_lpy, car_lpx)
         steering_angle = np.arctan2(2 * self.wheelbase_length * np.sin(ref_angle), self.lookahead)
 
+        # Publish steering angle
+        steering_angle_float = Float32()
+        steering_angle_float.data = steering_angle
+        self.steering_angle_pub.publish(steering_angle_float)
+
         drive_msg = AckermannDriveStamped()
         drive_msg.header.stamp = self.get_clock().now().to_msg()
         drive_msg.drive.steering_angle = steering_angle
-        drive_msg.drive.speed = self.speed
+        drive_msg.drive.speed = speed
         self.drive_pub.publish(drive_msg)
 
 
@@ -150,6 +195,9 @@ class PurePursuit(Node):
         self.trajectory.fromPoseArray(msg)
         self.trajectory.publish_viz(duration=0.0)
 
+        if len(self.trajectory.points) == 1:
+            self.trajectory.points.append(self.trajectory.points[0])
+ 
         self.starting_points = np.array(self.trajectory.points[:-1]) # (N, 2) list of tuples
         self.ending_points = np.array(self.trajectory.points[1:]) # (N, 2) list of tuples
         self.segments = self.ending_points - self.starting_points
