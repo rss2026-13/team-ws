c09960accd7c5e59c2943045eed0c7e5fe0cf84f
Muktha Ramesh <muktharamesh20@gmail.com>
2026-04-18 23:27:23 -0400

    Added Lane Detection

---
+++ src/lane_follower/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = 'lane_follower'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='RSS Team',
+    maintainer_email='todo@mit.edu',
+    description='Lane detection node for Final Challenge Part A',
+    license='TODO',
+    entry_points={
+        'console_scripts': [
+            'lane_follower_node = lane_follower.lane_follower_node:main',
+        ],
+    },
+)
